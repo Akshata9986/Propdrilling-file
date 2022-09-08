b6dc4e574bb965b5bd878bff4647be80a1914509
Akshata9986 <akshatanaik9164@gmail.com>
2022-09-08 12:12:41 +0530

    propdrilling file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import ComponentA from './Components/ComponentA';
+import { useState } from 'react';
 
 function App() {
+  let userName = "Akshata";
+  let [data, setData] = useState("Akshata");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data}</h1>
+      <ComponentA userName={userName} data={data} setData={setData} />
     </div>
   );
 }
